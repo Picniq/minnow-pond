eef110604aa2e6b023832b536f438d9c021b77d7
dperk6 <daniel@perklin.ca>
2021-12-31 14:04:29 -0500

    add required functions

--- contracts/interfaces/IUniswapRouter.sol
+++ contracts/interfaces/IUniswapRouter.sol
@@ -7,6 +7,18 @@ interface IUniswapRouter {
     // solhint-disable-next-line func-name-mixedcase
     function WETH() external pure returns (address);
 
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
+
     function swapExactETHForTokensSupportingFeeOnTransferTokens(
         uint256 amountOutMin,
         address[] calldata path,
